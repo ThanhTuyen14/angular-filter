7d4772b8000fff65fae4527b3bdd7a4b8d9b4d0e
Thanh Tuyen <tuyen.kyanon@gmail.com>
2017-11-03 15:13:46 +0700

    filter

--- demo-filter/assets/js/custom.js
+++ demo-filter/assets/js/custom.js
@@ -1,56 +1,30 @@
-// angular.module('demoFilter', []).controller('adminControl', function($scope) {
-//   $scope.sample_db = [{
-//       title: 'Food Kitchen',
-//       id: 'food',
-//       contents: [
-//         { name: 'Texas' },
-//         { name: 'KFC' },
-//         { name: 'Lotteria' }
-//       ],
-//     },
-//     {
-//       title: 'Bar/Pub/Coffee',
-//       id: 'bar',
-//       contents: [
-//         { name: 'GongCha' },
-//         { name: 'KOI' }
-//       ]
-//     },
-//     {
-//       title: 'Bathroom',
-//       id: 'bathroom',
-//       contents: [
-//         { name: 'A1' },
-//         { name: 'B2' }
-//       ]
-//     }
-//   ];
-//   // $scope.categoryList = Object.keys($scope.sample_db);
-//   // console.log($scope.categoryList);
-//   // $scope.getRecordByCategory = [];
-//   // var t = function(category) {
-//   //   return $scope.sample_db[category];
-//   // }
-//   $scope.getRecordByCategory = function (category) {
-//     return $scope.sample_db[category];
-//   }
-// });
-
-
 angular.module('demoFilter2', []).controller('adminControl2', function($scope) {
-  $scope.food = [
-    {name: 'Texas'},
-    {name: 'KFC'},
-    {name: 'Lotteria'}
+    $scope.sample_db = [{
+      title: 'Food Kitchen',
+      id: 'food',
+      contents: [
+        { name: 'banana' },
+        { name: 'banana is best' },
+        { name: 'love banana' }
+      ],
+    },
+    {
+      title: 'Bar/Pub/Coffee',
+      id: 'bar',
+      contents: [
+        { name: 'banana ba ba na na' },
+        { name: 'banana seven color' }
+      ]
+    },
+    {
+      title: 'Bathroom',
+      id: 'bathroom',
+      contents: [
+        { name: 'A1' },
+        { name: 'B2' }
+      ]
+    }
   ];
-  $scope.bar = [
-    {name: 'GongCha'},
-    {name: 'KOI'},
-    {name: 'Doremon'}
-  ];
-  $scope.bathroom = [
-    {name: 'A1'},
-    {name: 'B2'},
-    {name: 'C3'}
-  ];
+  
 });
+
